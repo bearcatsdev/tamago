4f6957f25a1daf2b033343f4fb1dc9e30e18900e
Chandra <developerchan1@gmail.com>
2019-07-26 00:46:13 +0700

    Mission fix position

--- app/src/main/java/com/bearcats/tamago/Task.java
+++ app/src/main/java/com/bearcats/tamago/Task.java
@@ -8,9 +8,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,6 +31,7 @@ public class Task extends Fragment {
 
     RecyclerView recyclerView;
     ArrayList<Task_Model> task;
+    JSONObject jsonObject;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,37 @@ public class Task extends Fragment {
         recyclerView.setHasFixedSize(true);
 
         task = new ArrayList<>();
+
+//        try{
+//            jsonObject.put("parent_id",1);
+//            jsonObject.put("child_name",childName.getText().toString());
+//            jsonObject.put("child_dob",childDob.getText().toString());
+//            jsonObject.put("child_initial_saving",childInitialSaving.getText().toString());
+//            jsonObject.put("child_daily_limit",childDailyLimit.getText().toString());
+//            jsonObject.put("child_avatar_type",1);
+//            jsonObject.put("child_gender",1);
+//            jsonObject.put("parent_relation",parentRelation.getText().toString());
+//        }catch(JSONException e){
+//            e.printStackTrace();
+//        }
+//
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, "https://raw.githubusercontent.com/BinusBearcats/TamagoFiles/master/test.json", jsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getContext(), response.toString()+"", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(getContext(), error.toString()+"", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        Volley.newRequestQueue(getContext()).add(jsonObjectRequest);
 
         task.add( new Task_Model(1,2,0,100000,"Vacuum the House","2 minute(s) ago"));
         task.add(new Task_Model(2,3,10,0,"Wash Car","10 minute(s) ago"));
