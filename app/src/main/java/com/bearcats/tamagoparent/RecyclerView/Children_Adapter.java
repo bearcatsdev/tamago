686b0367d239ac824fdba243e9e5e0285e50012c
Chandra <developerchan1@gmail.com>
2019-07-26 19:00:58 +0700

    add Children_Adapter and Children_Model

--- app/src/main/java/com/bearcats/tamagoparent/RecyclerView/Children_Adapter.java
+++ app/src/main/java/com/bearcats/tamagoparent/RecyclerView/Children_Adapter.java
@@ -1,4 +1,60 @@
 package com.bearcats.tamagoparent.RecyclerView;
 
-public class Children_Adapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bearcats.tamagoparent.R;
+import com.bearcats.tamagoparent.views.FButton;
+import com.sasank.roundedhorizontalprogress.RoundedHorizontalProgressBar;
+
+import java.util.ArrayList;
+
+public class Children_Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    ArrayList<Children_Model> children_models;
+    Context context;
+
+    public Children_Adapter(Context context, ArrayList<Children_Model> children_models){
+        this.context = context;
+        this.children_models = children_models;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.children_layout,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return children_models.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView name,goal;
+        ImageView child_icon;
+        RoundedHorizontalProgressBar progressBar;
+        FButton View, Activity, AddMission;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            
+        }
+    }
 }
---
+++ app/src/main/java/com/bearcats/tamagoparent/RecyclerView/Children_Model.java
@@ -0,0 +1,54 @@
+package com.bearcats.tamagoparent.RecyclerView;
+
+public class Children_Model {
+    private String name;
+    private int goal_start, goal_end,wallet, saving,egg;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGoal_start() {
+        return goal_start;
+    }
+
+    public void setGoal_start(int goal_start) {
+        this.goal_start = goal_start;
+    }
+
+    public int getGoal_end() {
+        return goal_end;
+    }
+
+    public void setGoal_end(int goal_end) {
+        this.goal_end = goal_end;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
+
+    public int getSaving() {
+        return saving;
+    }
+
+    public void setSaving(int saving) {
+        this.saving = saving;
+    }
+
+    public int getEgg() {
+        return egg;
+    }
+
+    public void setEgg(int egg) {
+        this.egg = egg;
+    }
+}
