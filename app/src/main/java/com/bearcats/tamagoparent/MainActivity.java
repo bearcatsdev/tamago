13a91eae32431be6b31ee78a6877b134b9a9b5b1
Stanley <angstanley68@gmail.com>
2019-07-27 11:24:21 +0700

    Revert code to how it supposed to work

--- app/src/main/java/com/bearcats/tamagoparent/MainActivity.java
+++ app/src/main/java/com/bearcats/tamagoparent/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (UserPreferences.getUserLoggedIn(this)) {
+        if (!UserPreferences.getUserLoggedIn(this)) {
             startActivity(new Intent(this, LoginActivity.class));
             finish();
         } else {
