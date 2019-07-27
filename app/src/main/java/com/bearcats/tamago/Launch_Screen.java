4de49b0122f27736a8ca9a81273cd55df1657f60
Chandra <developerchan1@gmail.com>
2019-07-27 11:30:10 +0700

    add camera permission

--- app/src/main/java/com/bearcats/tamago/Launch_Screen.java
+++ app/src/main/java/com/bearcats/tamago/Launch_Screen.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bearcats.tamago.Activity.Login;
 import com.bearcats.tamago.Activity.MainActivity;
+import com.bearcats.tamago.preferences.ChildPreferences;
 
 public class Launch_Screen extends AppCompatActivity {
 
@@ -12,7 +14,16 @@ public class Launch_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
+
+        Intent intent;
+        if(!ChildPreferences.getChildLoggedIn(this)){
+            intent = new Intent(this, Login.class);
+        }
+        else
+        {
+            intent = new Intent(this,MainActivity.class);
+        }
+
         startActivity(intent);
         finish();
     }
